10682559de2fbb4e2bdedbf442000b43e71138ed
behzad <42198604+behzadeskandari@users.noreply.github.com>
2022-12-17 11:39:11 +0330

    Add project files.

---
+++ HotelListing.Core/Repository/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using HotelListing.Core.IRepository;
+using HotelListing.Core.Model;
+using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace HotelListing.Core.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<Country> Countries => _countries ??= new GenericRepository<Country>(_context);
+        public IGenericRepository<Hotel> Hotels => _hotels ??= new GenericRepository<Hotel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ HotelListing.Core/Services/IAuthManager.cs
@@ -0,0 +1,18 @@
+﻿using HotelListing.Core.DTOs;
+using HotelListing.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.Core.Services
+{
+    public interface IAuthManager
+    {
+        Task<bool> ValidateUser(LoginUserDTO userDTO);
+        Task<string> CreateToken();
+        Task<string> CreateRefreshToken();
+        Task<TokenRequest> VerifyRefreshToken(TokenRequest request);
+    }
+}
